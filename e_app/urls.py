aabfb282a3551d7f60e529af01889ee3ca115fd7
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-08-22 20:52:44 +0300

    statucfiles/admin/css/vendor/select2/LICENSE-SELECT2.md

--- e_app/urls.py
+++ e_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'e_app'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about_view, name='about'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -1,22 +1,14 @@
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os   
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-70w6h-3g3!=$#qs4qst_e5vf-x*58)f6_hr##)*z*%yma7tawh'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,11 +17,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orders.apps.OrdersConfig',     # Add this line for the 'orders' app
-    'cart.apps.CartConfig',         # Add this line for the 'cart' app
-    'products.apps.ProductsConfig', # Add this line for the 'products' app
-    'e_app.apps.EAppConfig',        # Add this line for the 'e_app' app
-    'payment.apps.PaymentConfig',   # Add this line for the 'payment' app
+    'orders.apps.OrdersConfig',     
+    'cart.apps.CartConfig',        
+    'products.apps.ProductsConfig', 
+    'e_app.apps.EAppConfig',        
+    'payment.apps.PaymentConfig',   
 ]
 
 MIDDLEWARE = [
@@ -61,22 +53,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'e_commerce.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -92,10 +74,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -105,12 +83,22 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'e_app/static'),
+    os.path.join(BASE_DIR, 'payment/static'),
+    os.path.join(BASE_DIR, 'products/static'),
+    os.path.join(BASE_DIR, 'cart/static'),
+    os.path.join(BASE_DIR, 'orders/static'),
+    
+]
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
